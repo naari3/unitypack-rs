69937c3253969a4e9f8b2378f3bf4d259c7e7de8
naari3 <naari.named@gmail.com>
2021-07-12 00:39:44 +0900

    Rename from Asset to AssetBundle

--- src/lib.rs
+++ src/lib.rs
@@ -10,62 +10,75 @@ use nom::{
 };
 
 #[derive(Debug)]
-struct UnityAsset {
-    header: UnityAssetHeader,
-    container_header: UnityContainerHeader,
-    storage_block: Vec<UnityStorageBlock>,
-    directory_info: Vec<UnityNode>,
+pub struct UnityAssetBundle {
+    pub header: UnityAssetBundleHeader,
+    pub container_header: UnityContainerHeader,
+    pub storage_blocks: Vec<UnityStorageBlock>,
+    pub directory_info: Vec<UnityNode>,
+    pub stream_files: Vec<UnityStreamFile>,
 }
 
-#[derive(Debug)]
-struct UnityAssetHeader {
-    signature: String,
-    version: u32,
-    unity_version: String,
-    unity_revision: String,
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord)]
+pub struct UnityAssetBundleHeader {
+    pub signature: String,
+    pub version: u32,
+    pub unity_version: String,
+    pub unity_revision: String,
 }
 
-#[derive(Debug, Default)]
-struct UnityContainerHeader {
-    size: i64,
-    compressed_blocks_info_size: u32,
-    uncompressed_blocks_info_size: u32,
-    flags: u32,
+#[derive(Debug, Default, PartialEq, Eq, PartialOrd, Ord)]
+pub struct UnityContainerHeader {
+    pub size: i64,
+    pub compressed_blocks_info_size: u32,
+    pub uncompressed_blocks_info_size: u32,
+    pub flags: u32,
 }
 
-#[derive(Debug, Default)]
-struct UnityStorageBlock {
-    compressed_size: u32,
-    uncompressed_size: u32,
-    flags: u16,
+#[derive(Debug, Default, PartialEq, Eq, PartialOrd, Ord)]
+pub struct UnityStorageBlock {
+    pub compressed_size: u32,
+    pub uncompressed_size: u32,
+    pub flags: u16,
 }
 
-#[derive(Debug, Default)]
-struct UnityNode {
-    offset: i64,
-    size: i64,
-    flags: u32,
-    path: String,
+#[derive(Debug, Default, PartialEq, Eq, PartialOrd, Ord)]
+pub struct UnityNode {
+    pub offset: i64,
+    pub size: i64,
+    pub flags: u32,
+    pub path: String,
 }
 
-fn read_unity_asset(input: &[u8]) -> IResult<&[u8], UnityAsset> {
-    let (input, unity_asset_header) = read_unity_asset_header(input)?;
+#[derive(Debug, Default, PartialEq, Eq, PartialOrd, Ord)]
+pub struct UnityStreamFile {
+    pub path: String,
+    pub file_name: String,
+    pub body: Vec<u8>,
+}
+
+pub fn read_unity_asset_bundle(input: &[u8]) -> IResult<&[u8], UnityAssetBundle> {
+    let (input, unity_asset_bundle_header) = read_unity_asset_bundle_header(input)?;
     let (input, unity_container_header) = read_unity_container_header(input)?;
-    let (input, ((unity_asset_header, unity_container_header), (storage_block, directory_info))) =
-        read_blocks_info_and_directory(input, unity_asset_header, unity_container_header)?;
+    let (
+        input,
+        ((unity_asset_bundle_header, unity_container_header), (storage_blocks, directory_info)),
+    ) = read_blocks_info_and_directory(input, unity_asset_bundle_header, unity_container_header)?;
+    let (input, (stream_files, storage_blocks, directory_info)) =
+        read_files(input, storage_blocks, directory_info)?;
 
     Ok((
         input,
-        UnityAsset {
-            header: unity_asset_header,
+        UnityAssetBundle {
+            header: unity_asset_bundle_header,
             container_header: unity_container_header,
-            storage_block,
+            storage_blocks,
             directory_info,
+            stream_files,
         },
     ))
 }
 
-fn read_unity_asset_header(input: &[u8]) -> IResult<&[u8], UnityAssetHeader> {
+fn read_unity_asset_bundle_header(input: &[u8]) -> IResult<&[u8], UnityAssetBundleHeader> {
     let (input, signature) = alpha1(input)?;
     let (input, _) = tag(b"\0")(input)?;
     let signature = std::str::from_utf8(signature).unwrap().to_string();
@@ -82,7 +95,7 @@ fn read_unity_asset_header(input: &[u8]) -> IResult<&[u8], UnityAssetHeader> {
 
     Ok((
         input,
-        UnityAssetHeader {
+        UnityAssetBundleHeader {
             signature,
             version,
             unity_version,
@@ -110,12 +123,12 @@ fn read_unity_container_header(input: &[u8]) -> IResult<&[u8], UnityContainerHea
 
 fn read_blocks_info_and_directory(
     input: &[u8],
-    header: UnityAssetHeader,
+    header: UnityAssetBundleHeader,
     container_header: UnityContainerHeader,
 ) -> IResult<
     &[u8],
     (
-        (UnityAssetHeader, UnityContainerHeader),
+        (UnityAssetBundleHeader, UnityContainerHeader),
         (Vec<UnityStorageBlock>, Vec<UnityNode>),
     ),
 > {
@@ -132,23 +145,27 @@ fn read_blocks_info_and_directory(
         // kArchiveBlocksAndDirectoryInfoCombined
         take(container_header.compressed_blocks_info_size)(input)?
     };
-    let block_info = match container_header.flags & 0x3F {
-        //kArchiveCompressionTypeMask
-        1 => todo!(), // LZMA
-        2 | 3 => {
-            let decoded = lz4_flex::block::decompress(
-                compressed_blocks_info_bytes,
-                container_header.uncompressed_blocks_info_size as usize,
-            )
-            .unwrap();
-            decoded
-        } // LZ4, LZ4HC
-        _ => (compressed_blocks_info_bytes.to_owned().to_vec()), // None
-    };
+    let block_info = decompress(
+        compressed_blocks_info_bytes,
+        container_header.uncompressed_blocks_info_size as usize,
+        container_header.flags,
+    );
     // let block_info = block_info.as_slice();
     let (_block_info, (storage_blocks, nodes)) = read_block_infos(&block_info).unwrap();
 
     Ok((input, ((header, container_header), (storage_blocks, nodes))))
+}
+
+fn decompress(compressed_bytes: &[u8], uncompressed_size: usize, flags: u32) -> Vec<u8> {
+    match flags & 0x3F {
+        //kArchiveCompressionTypeMask
+        1 => todo!(), // LZMA
+        2 | 3 => {
+            let decoded = lz4_flex::block::decompress(compressed_bytes, uncompressed_size).unwrap();
+            decoded
+        } // LZ4, LZ4HC
+        _ => (compressed_bytes.to_owned().to_vec()), // None
+    }
 }
 
 fn read_block_infos(block_info: &[u8]) -> IResult<&[u8], (Vec<UnityStorageBlock>, Vec<UnityNode>)> {
@@ -198,11 +215,70 @@ fn read_node(input: &[u8]) -> IResult<&[u8], UnityNode> {
     ))
 }
 
+fn read_files(
+    input: &[u8],
+    storage_blocks: Vec<UnityStorageBlock>,
+    directory_info: Vec<UnityNode>,
+) -> IResult<&[u8], (Vec<UnityStreamFile>, Vec<UnityStorageBlock>, Vec<UnityNode>)> {
+    let (input, (decompressed, storage_blocks)) =
+        decompress_by_storage_blocks(input, storage_blocks)?;
+    let (_decompressed, (stream_files, directory_info)) =
+        read_stream_files(&decompressed, directory_info).unwrap();
+    Ok((input, (stream_files, storage_blocks, directory_info)))
+}
+
+fn decompress_by_storage_blocks(
+    input: &[u8],
+    storage_blocks: Vec<UnityStorageBlock>,
+) -> IResult<&[u8], (Vec<u8>, Vec<UnityStorageBlock>)> {
+    let mut input = input;
+    let mut decompresseds = Vec::with_capacity(storage_blocks.len());
+    let mut dec = vec![];
+    for sb in storage_blocks.iter() {
+        let compressed_bytes: &[u8];
+        let result = take(sb.compressed_size)(input)?;
+        input = result.0;
+        compressed_bytes = result.1;
+        let decompressed = decompress(
+            compressed_bytes,
+            sb.uncompressed_size as usize,
+            sb.flags as u32,
+        );
+        decompresseds.push(decompressed);
+    }
+    for mut d in decompresseds {
+        dec.append(&mut d);
+    }
+    Ok((input, (dec, storage_blocks)))
+}
+
+fn read_stream_files(
+    input: &[u8],
+    directory_info: Vec<UnityNode>,
+) -> IResult<&[u8], (Vec<UnityStreamFile>, Vec<UnityNode>)> {
+    let mut input = input;
+    let mut stream_files = vec![];
+    for di in directory_info.iter() {
+        let body: &[u8];
+        let result = take(di.size as usize)(input)?;
+        input = result.0;
+        body = result.1;
+        let sf = UnityStreamFile {
+            path: di.path.clone(),
+            file_name: di.path.clone(),
+            body: body.to_vec(),
+        };
+        stream_files.push(sf);
+    }
+
+    Ok((input, (stream_files, directory_info)))
+}
+
 #[cfg(test)]
 mod tests {
     use std::{io::Read, path::Path};
 
-    use crate::read_unity_asset;
+    use crate::read_unity_asset_bundle;
 
     fn read_file<P: AsRef<Path>>(file_path: P) -> Vec<u8> {
         let mut file = std::fs::File::open(file_path).expect("file open failed");
@@ -212,28 +288,34 @@ mod tests {
     }
 
     #[test]
-    fn test_read_unity_asset_header() {
-        let file = read_file("./anm_chara_tear_animator");
-        let unity_asset = read_unity_asset(&file).unwrap().1;
-        println!("{:?}", unity_asset);
-        assert_eq!("UnityFS", unity_asset.header.signature);
-        assert_eq!(6, unity_asset.header.version);
-        assert_eq!("5.x.x", unity_asset.header.unity_version);
-        assert_eq!("2019.4.1f1", unity_asset.header.unity_revision);
-        assert_eq!(9185, unity_asset.container_header.size);
-        assert_eq!(65, unity_asset.container_header.compressed_blocks_info_size);
+    fn test_read_unity_asset_bundle_header() {
+        let file = read_file("./item_icon_00000");
+        let unity_asset_bundle = read_unity_asset_bundle(&file).unwrap().1;
+        assert_eq!("UnityFS", unity_asset_bundle.header.signature);
+        assert_eq!(6, unity_asset_bundle.header.version);
+        assert_eq!("5.x.x", unity_asset_bundle.header.unity_version);
+        assert_eq!("2019.4.1f1", unity_asset_bundle.header.unity_revision);
+        assert_eq!(4465, unity_asset_bundle.container_header.size);
         assert_eq!(
-            91,
-            unity_asset.container_header.uncompressed_blocks_info_size
+            85,
+            unity_asset_bundle
+                .container_header
+                .compressed_blocks_info_size
         );
-        assert_eq!(67, unity_asset.container_header.flags);
-        assert_eq!(1, unity_asset.directory_info.len());
-        assert_eq!(0, unity_asset.directory_info[0].offset);
-        assert_eq!(34932, unity_asset.directory_info[0].size);
-        assert_eq!(4, unity_asset.directory_info[0].flags);
         assert_eq!(
-            "CAB-f946f47e8f8bb3ec3f2f2259084955c0",
-            unity_asset.directory_info[0].path
+            153,
+            unity_asset_bundle
+                .container_header
+                .uncompressed_blocks_info_size
+        );
+        assert_eq!(67, unity_asset_bundle.container_header.flags);
+        assert_eq!(2, unity_asset_bundle.directory_info.len());
+        assert_eq!(0, unity_asset_bundle.directory_info[0].offset);
+        assert_eq!(4512, unity_asset_bundle.directory_info[0].size);
+        assert_eq!(4, unity_asset_bundle.directory_info[0].flags);
+        assert_eq!(
+            "CAB-5813386f0ea15049abeb5a688d9031d3",
+            unity_asset_bundle.directory_info[0].path
         );
     }
 }
